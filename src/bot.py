4e69c4a505441fc692143ee3924361767dd63cf4
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-02-07 17:12:55 -0500

    Gemini api integration
    Beta Gemini chat powered with ai

--- src/bot.py
+++ src/bot.py
@@ -40,6 +40,7 @@ DISCORD_CHANNEL =
 GENERAL_CHANNEL = 
 YOUTUBE_API_KEY = ''
 SEARCH_API_KEY = ''
+GEMINI_APIKEY = ''
 CSE_ID = ''
 SERVER_ID = ''
 CHANNEL_ID = ''
@@ -55,6 +56,10 @@ last_live_id = None
 
 pytesseract.pytesseract.tesseract_cmd = r'/home/container/Tesseract-OCR/tesseract.exe'
 youtube = googleapiclient.discovery.build('youtube', 'v3', developerKey = '')
+genai.configure(api_key= GEMINI_API_KEY)
+#configuración de Gemini
+model = genai.GenerativeModel("gemini-1.5-flash")
+chat = model.start_chat(history=[])
 
 @bot.event
 async def on_ready():
@@ -747,6 +752,34 @@ async def translateinfo(ctx):
     )
 
     await ctx.send(embed=embed)
+
+@bot.command()
+async def ia(ctx, *, mensaje: str = None):
+    if not mensaje:
+        await ctx.send('Escribe algo para preguntarle a la IA de Gemini.')
+        return
+    try:
+        if ctx.message.attachments:
+            image_url = ctx.message.attachments[0].url
+            img_response = requests.get(image_url)
+            image_bytes = img_response.content
+
+            image = Image.open(BytesIO(image_bytes))
+
+            img_iaresponse = model.generate_content([mensaje, image])
+
+            text = img_iaresponse.text
+            
+            for i in range(0, len(text), 2000):
+                await ctx.send(text[i:i+2000])
+        else:
+            response = model.generate_content(mensaje)
+            text = response.text
+            for i in range(0, len(text), 2000):
+                await ctx.send(text[i:i+2000])
+    except Exception as e:
+        await ctx.send('Hubo un error al generar una respuesta:')
+        print(e)
 
 @bot.event
 async def on_command_error(ctx, error):
